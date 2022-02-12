fb9aed6af3a817c9747452eac87a1ac5b8cffc6d
jcelisca <celiss1997@gmail.com>
2022-02-12 10:10:33 -0500

    Creación de la interface entregable

---
+++ ejercicio17/ejercicio18/Entregable.java
@@ -0,0 +1,8 @@
+package ejercicio17.ejercicio18;
+
+public interface Entregable {
+    public void entregar();
+    public void devolver();
+    public boolean isEntregado();
+    public void compareTo(Object a);
+}
